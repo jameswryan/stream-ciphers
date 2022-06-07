fd874f045e12b396ae52b585514fb1cccaa5c149
James Ryan <james@wryan.dev>
2022-06-06 21:10:31 -0600

    sse2-backend

--- salsa20/src/backends.rs
+++ salsa20/src/backends.rs
@@ -5,7 +5,15 @@ cfg_if! {
         pub(crate) mod soft;
     } else if #[cfg(all(target_arch = "aarch64", target_feature = "neon"))] {
         pub(crate) mod neon;
-
+    } else if #[cfg(any(target_arch = "x86", target_arch  ="x86_64"))] {
+        cfg_if! {
+            if #[cfg(salsa20_force_sse2)] {
+                pub(crate) mod sse2;
+            } else {
+                pub(crate) mod soft;
+                pub(crate) mod sse2;
+            }
+        }
     } else {
         pub(crate) mod soft;
     }
